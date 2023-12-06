91248b00092871f70f614a3e881188500ef64116
MOHAMMED TOUFEEQ <129126839+MOHDTOUFEEQ@users.noreply.github.com>
2023-12-06 14:28:10 +0000

    Sign in session

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,34 @@ import './App.css'
 import { Header } from './components/header/Header'
 import Footer from './components/footer/Footer'
 import { Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { login } from './store/authSlice'
 
 
 function App() {
-  const data = useSelector((state)=> state.auth)
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+     </>
+        ) : null
 }
 
 export default App
